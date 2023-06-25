6fdce4623d2fdbfe3c282d455f45820947095914
Dozie2001 <davidchidozie402@gmail.com>
2023-06-25 10:58:35 +0100

    Send json objects using routes

--- myApp.js
+++ myApp.js
@@ -1,13 +1,34 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
-
-
+let response = ''
+const signal = (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+}
+/* app.get("/", signal); */
 console.log('Hello World');
 
-
 app.get("/", (req, res) => {
-    res.send("Hello Express")
+  res.sendFile(__dirname + "/views/index.html");
 })
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    response = 'Hello json'.toUpperCase();
+  }else {
+    response = 'Hello json'
+  }
+    res.json({
+      "message": response
+    })
+});
+
+
+
+
+
 
 
 
@@ -36,3 +57,20 @@ app.get("/", (req, res) => {
 
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
